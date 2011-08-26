2bd90e7affba36191510870ff29c6d473639c2b5
Caleb Cushing <xenoterracide@gmail.com>
2011-08-25 22:14:20 -0500

    first cc type implementation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Types/CreditCard.pm
+++ lib/MooseX/Types/CreditCard.pm
@@ -1,10 +1,26 @@
 package MooseX::Types::CreditCard;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
+use MooseX::Types -declare => [ qw( CreditCard ) ];
+use MooseX::Types::Moose qw( Str Int );
+use namespace::autoclean;
+
+use Business::CreditCard;
+
+subtype CreditCard,
+	as Int,
+	where { validate($_) },
+	message { $_ . ' is not a valid credit card number' }
+	;
+
+coerce CreditCard,
+	from Str,
+	via { my $int = $_ =~ /([[:digit:]])/gxms }
+	;
 
 1;
 
